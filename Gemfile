6bf1215962ad3cf03aa52ac9153d20f2909ae6b3
Your Name <github@misel.net>
2011-04-14 17:26:55 +0200

    first version (not working)

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'http://rubygems.org'
 
 gem 'sinatra', '1.0.0'
+gem 'thin'
+gem 'haml'
+gem 'zanox', :git => 'https://github.com/kr1sp1n/zanox.git', :branch => 'master'
--- app.rb
+++ app.rb
@@ -1,6 +1,46 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
+require 'zanox'
 
-get '/hi' do
-  "Hello World!"
+
+
+configure do
+  $zanox_connect_link='https://auth.zanox-affiliate.de/login?appid=1A954C44E978336DABF8'
+  Zanox::API.public_key = PUBLIC_KEY
+  Zanox::API.secret_key = SECRET_KEY
+end
+
+get '/' do
+  if Zanox::API::Session.connect_id.nil?
+    @connected = false
+  else
+    @connected = true
+  end
+  haml :index
+end
+
+get '/auth' do
+  #login here
+  if(params[:authtoken]) 
+    Zanox::API::Session.new(params[:authtoken])
+    redirect '/apps'
+  else 
+    @error = 'Login unsuccessful - could not get session'
+    haml :nosuccess
+  end
+end
+
+get '/apps' do
+  #Zanox::API::
+  unless Zanox::API::Session.connect_id.nil?
+    @page = (params[:page] || 1).to_i
+    @admedia = Zanox::Admedium.find(:all ) # , :items=>10, :page=> @page-1, partnership => 'direct', purpose=>'productdeeplink'
+  end
+  unless @admedia.nil?
+    haml :admedia
+  else
+    @error = 'Admedia could not be accessed'
+    haml :nosuccess
+  end
 end
