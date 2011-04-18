c3e9561e0302c46302e135dd1c2c8a37a31d8b2f
Your Name <github@misel.net>
2011-04-18 15:28:10 +0200

    autosave

--- app.rb
+++ app.rb
@@ -4,11 +4,19 @@ require 'haml'
 require 'zanox'
 require '../zapoddilg_keys'
 
+class PartnerData
+  attr_reader :adspace, :program
+  def initialize(adspace, program)
+    @adspace = adspace
+    @program = program
+  end
+end
 
 configure do
   $zanox_connect_link='https://auth.zanox-affiliate.de/login?appid=1A954C44E978336DABF8'
   Zanox::API.public_key = PUBLIC_KEY
   Zanox::API.secret_key = SECRET_KEY
+  PAGE_SIZE = 50
 end
 
 get '/' do
@@ -24,7 +32,7 @@ get '/auth' do
   #login here
   if(params[:authtoken]) 
     Zanox::API::Session.new(params[:authtoken])
-    redirect '/apps'
+    redirect '/'
   else 
     @error = 'Login unsuccessful - could not get session'
     haml :nosuccess
@@ -34,13 +42,39 @@ end
 get '/apps' do
   #Zanox::API::
   unless Zanox::API::Session.connect_id.nil?
-    @programApplications = Zanox::ProgramApplication.find(:all, :status => 'confirmed')
+    programApplications = Zanox::ProgramApplication.find(:all, :status => 'confirmed', :items => PAGE_SIZE)
+    myPrograms = []
+    myPrograms = myPrograms + programApplications
+
+    page = 1
+    while (programApplications.size > 0) do
+      programApplications = Zanox::ProgramApplication.find(:all, :status => 'confirmed', :items => PAGE_SIZE, :page => page)
+      page+=1
+      myPrograms=myPrograms + programApplications
+    end
+
+    partnerData = []
+    myPrograms.each{ |myProgram|
+      myPartnerData = PartnerData.new(myProgram.adspace.xmlattr_id, myProgram.program.xmlattr_id)
+      myPartnerData.inspect
+      partnerData = partnerData.push(myPartnerData)
+    }
+    
+    deeplinks = []
+    myPrograms.each do |myProgram|
+      admedia = Zanox::Admedium.find(:all , :purpose=>'productDeeplink', :program => myProgram.program.xmlattr_id, :adspace => myProgram.adspace.xmlattr_id)
+      deeplinks = deeplinks + admedia
+    end
+
+    
 #    @page = (params[:page] || 1).to_i
 #    @admedia = Zanox::Admedium.find(:all , :items=>5, :page=> @page-1, :purpose=>'productDeeplink', :partnerShip => 'direct')
 #    @admedia.inspect
+
   end
-  unless @programApplications.nil?
+  unless deeplinks.nil?
     @connected = true
+    @deeplinks = deeplinks
     haml :admedia
   else
     @error = 'Admedia could not be accessed'
@@ -55,4 +89,9 @@ end
 post '/input' do
   @url = params[:url]
   haml :input
+end
+
+get '/links' do
+  @admedia = Zanox::Admedium.find(:all , :purpose=>'productDeeplink', :program => 430, :adspace => 205575)
+  haml :links
 end
