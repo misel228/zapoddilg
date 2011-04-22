bd4b10d7bead9d56b89054dd2f149f51588f3cee
Your Name <github@misel.net>
2011-04-22 23:20:11 +0200

    autosave

--- app.rb
+++ app.rb
@@ -15,6 +15,13 @@ end
 
 get '/' do
   @connected = session[:connected]
+  @profile = session[:profile]
+  if(@connected)
+    profile = Zanox::Profile.find()[0]
+    @userId = session[:user_id] = profile.xmlattr_id
+    @userName = session[:user_name] = profile.userName
+    @firstName = session[:first_name] = profile.firstName
+  end
   haml :index
 end
 
@@ -22,10 +29,8 @@ get '/auth' do
   #login here
   if(params[:authtoken])
     Zanox::API::Session.new(params[:authtoken])
-#     puts Zanox::Session.getUiUrl()
     session[:connected]=true
-    #session[:ui_url] = 
-    redirect '/'
+    redirect '/input'
   else 
     @error = 'Login unsuccessful - could not get session'
     haml :nosuccess
@@ -60,6 +65,9 @@ get '/apps' do
 
   end
   unless deeplinks.nil?
+    @userId = session[:user_id]
+    @userName = session[:user_name]
+    @firstName = session[:first_name]
     @connected = session[:connected]
     @deeplinks = deeplinks
     @partnerCodes = partnerCodes
@@ -74,7 +82,13 @@ end
 get '/input' do
   @connected = session[:connected]
   if @connected
-    @ui_url = Zanox::Connect.ui_url()
+    @userId = session[:user_id]
+    @userName = session[:user_name]
+    @firstName = session[:first_name]
+    url = Zanox::Connect.ui_url()
+    old = 'z_in_frm.dll?1003100310030&'
+    new = 'z_in_frm.dll?1003400140010&'
+    @ui_url = url.url.to_s.sub(old, new)
   end
   @url=session[:url]
   haml :input
@@ -82,23 +96,52 @@ end
 
 post '/input' do
   @connected = session[:connected]
+  @userId = session[:user_id]
+  @userName = session[:user_name]
+  @firstName = session[:first_name]
   session[:url]=params[:url]
   @url=session[:url]
   @stored = true
+  redirect '/adspaces'
   haml :input
 end
 
 get '/links' do
   @connected = session[:connected]
+  @userId = session[:user_id]
+  @userName = session[:user_name]
+  @firstName = session[:first_name]
   @admedia = Zanox::Admedium.find(:all , :purpose=>'productDeeplink', :programId => 430.to_s, :adspaceId => 205575.to_s)
   haml :links
 end
 
 get '/adspaces' do
   @connected = session[:connected]
+  @userId = session[:user_id]
+  @userName = session[:user_name]
+  @firstName = session[:first_name]
   @adspaces = Zanox::Adspace.find(:all)
   haml :adspaces
 end
+
+
+post '/adspaces' do
+  session[:adspaces]=''
+  params.each do |param|
+    keyValue = param[0].split('_')
+    if(keyValue[1]=='adspace')
+      session[:adspaces]+= param[1] + ','
+    end
+  end
+  @connected = session[:connected]
+  @userId    = session[:user_id]
+  @userName  = session[:user_name]
+  @firstName = session[:first_name]
+  @adspaces  = Zanox::Adspace.find(:all)
+  @selectedAdspaces = session[:adspaces]
+  haml :adspaces
+end
+
 
 get '/logout' do
   @connected = session[:connected] = false
